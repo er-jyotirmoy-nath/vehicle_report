b63c2c242c54902feac0bbf87d4b10c7c53bb186
er.jyotirmoy.nath@gmail.com <anjana@123>
2017-04-02 18:39:15 +0530

    All Done

--- DB/vehicle_info.sql
+++ DB/vehicle_info.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2017 at 08:41 AM
+-- Generation Time: Apr 02, 2017 at 12:24 PM
 -- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -71,9 +71,8 @@ CREATE TABLE `maint_dtl` (
 --
 
 INSERT INTO `maint_dtl` (`veh_id`, `BA_no`, `oil_ch_dt`, `oil_ch_km`, `oil_ch_exp`, `air_filter_dt`, `air_filter_km`, `air_filter_exp`, `fuel_filter_dt`, `fuel_filter_km`, `fuel_filter_exp`, `bty_chg_dt`, `bty_chg_exp`, `steering_oil_dt`, `steering_oil_exp`) VALUES
-(2, 'DL 3C 0931', '2017-03-01', '2000', '2017-03-11', '2017-03-01', '2000', '2017-03-18', '2017-03-01', '2000', '2017-03-16', '2017-03-01', '2017-03-11', '2017-03-02', '2017-03-18'),
-(2, 'DL 3C 0931', '2017-03-11', '2000', '2017-03-15', '2017-03-12', '2000', '2017-03-25', '2017-03-11', '2000', '2017-03-19', '2017-03-05', '2017-03-23', '2017-03-11', '2017-03-25'),
-(3, 'DL 4C 3223', '2017-03-11', '2000', '2017-03-25', '2017-03-11', '2000', '2017-03-25', '2017-03-11', '2000', '2017-03-18', '2017-03-11', '2017-03-19', '2017-03-11', '2017-03-24');
+(2, 'DL 3C 0931', '2017-03-11', '2000', '2017-04-15', '2017-03-12', '2000', '2017-04-29', '2017-03-11', '2000', '2017-04-28', '2017-03-05', '2017-04-29', '2017-03-11', '2017-04-29'),
+(3, 'DL 4C 3223', '2017-03-11', '2000', '2017-03-29', '2017-03-11', '2000', '2017-03-25', '2017-03-11', '2000', '2017-03-18', '2017-03-11', '2017-03-19', '2017-03-11', '2017-03-24');
 
 -- --------------------------------------------------------
 
@@ -139,7 +138,8 @@ CREATE TABLE `vehicle_master` (
 
 INSERT INTO `vehicle_master` (`veh_id`, `BA_no`, `veh_type`, `dt_mfr`, `dt_induction`, `dt_takingover`, `class`, `km_engine`, `km_chassis`, `engine_no`, `chassis_no`) VALUES
 (2, 'DL 3C 0931', 'MC', '2012-09-02', '2015-09-02', '2017-02-01', '4', '2000', '2000', '2132321', '212121121'),
-(3, 'DL 4C 3223', 'MG', '2013-10-02', '2015-09-03', '2016-09-29', '4', '3000', '3000', '212423121', '23213413');
+(3, 'DL 4C 3223', 'MG', '2013-10-02', '2015-09-03', '2016-09-29', '4', '3000', '3000', '212423121', '23213413'),
+(4, 'DL 1C 4567', 'ALS', '2014-01-01', '2015-01-01', '2016-11-17', '2', '3000', '2000', '3323212', '2321312323');
 
 -- --------------------------------------------------------
 
@@ -185,12 +185,12 @@ ALTER TABLE `vehicle_master`
 -- AUTO_INCREMENT for table `tyre_dtls_table`
 --
 ALTER TABLE `tyre_dtls_table`
-  MODIFY `tyre_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2123232;
+  MODIFY `tyre_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21237;
 --
 -- AUTO_INCREMENT for table `vehicle_master`
 --
 ALTER TABLE `vehicle_master`
-  MODIFY `veh_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `veh_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
